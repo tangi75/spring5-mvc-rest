6143e6d7a9857d1226c6ddd41de2173f77bda802
Tangi Vass <tangi.vass.ep@biznet.io>
2019-01-12 16:39:45 +0100

    Create New Customer With Post

--- src/main/java/guru/springfamework/api/v1/model/CustomerDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/CustomerDTO.java
@@ -1,10 +1,14 @@
 package guru.springfamework.api.v1.model;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 @Data
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class CustomerDTO {
     private String firstname;
     private String lastname;
+    //@JsonProperty("customer_url")
     private String customerUrl;
 }
